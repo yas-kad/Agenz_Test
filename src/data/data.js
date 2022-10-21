93cb1ade6c393196d17938ec95a33aa641d87056
yas-kad <yassindeveloppeur2018@gmail.com>
2022-10-21 21:05:12 +0100

    fix index.html

--- src/data/data.js
+++ src/data/data.js
@@ -52,6 +52,54 @@ const data = [
         ville:"Tanger, Medina",
         annonce:"Annonce par : Tanger conseils Invest"
     },
+    {
+        id: '7',
+        CardImg:img1, 
+        Price:"1,110,122",
+        Content:"Maison à vendre | 3 chambres | 230 m²",
+        ville:"Marrakech, Ennakhil (Palmeraie)",
+        annonce:"Annonce par : Marrakech conseils Invest"
+    },
+    {
+        id: '8',
+        CardImg:img2, 
+        Price:"1,110,122",
+        Content:"Appartement à vendre | 2 chambres | 140 m²",
+        ville:"Casablanca, Anfa",
+        annonce:"Annonce par : casa conseils Invest  "
+    },
+    {
+        id: '9',
+        CardImg:img3, 
+        Price:"2,110,122",
+        Content:"Maison à vendre | 3 chambres | 230 m²",
+        ville:"Agadir,  Say Salam",
+        annonce:"Annonce par : agadir conseils Invest"
+    },
+    {
+        id: '10',
+        CardImg:img4, 
+        Price:"1,500,122",
+        Content:"Maison à vendre | 3 chambres | 230 m²",
+        ville:"Tanger, Medina",
+        annonce:"Annonce par : Tanger conseils Invest"
+    },
+    {
+        id: '11',
+        CardImg:img2, 
+        Price:"1,110,122",
+        Content:"Appartement à vendre | 2 chambres | 140 m²",
+        ville:"Casablanca, Anfa",
+        annonce:"Annonce par : casa conseils Invest  "
+    },
+    {
+        id: '12',
+        CardImg:img4, 
+        Price:"1,500,122",
+        Content:"Maison à vendre | 3 chambres | 230 m²",
+        ville:"Tanger, Medina",
+        annonce:"Annonce par : Tanger conseils Invest"
+    },
 ]
 
 export default data;
